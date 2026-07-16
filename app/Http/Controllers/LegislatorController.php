7089cbc171626a11447cf65da7b861b3f67a793e
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-16 15:17:01 -0300

    Adiciona métodos indexForDeputies, indexForSenators e show no LegislatorController

--- app/Http/Controllers/LegislatorController.php
+++ app/Http/Controllers/LegislatorController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class LegislatorController extends Controller
 {
-    //
+    public function indexForDeputies() {
+        return response()->json();
+    }
+
+    public function indexForSenators() {
+        return response()->json();
+    }
+
+    public function show() {
+        return response()->json();
+    }
 }
