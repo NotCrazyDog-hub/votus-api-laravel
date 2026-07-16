83b77296c8611b9df1b499dbeb65470be611e7e3
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-16 15:01:16 -0300

    Adiciona propriedades fillable e casts na model Legislator

--- app/Models/Legislator.php
+++ app/Models/Legislator.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Legislator extends Model
 {
-    //
+    protected $fillable = [
+        'external_id',
+        'chamber',
+        'civil_name',
+        'parliamentary_name',
+        'photo_url',
+        'party',
+        'state',
+        'legislature',
+        'electoral_status',
+        'status',
+        'phone',
+        'email',
+        'official_website',
+        'social_media',
+        'raw_data',
+    ];
+
+    protected $casts = [
+        'social_media' => 'array',
+        'raw_data' => 'array',
+    ];
 }
