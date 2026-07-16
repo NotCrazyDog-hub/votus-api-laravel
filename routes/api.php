4cd3f07baf4394f6338305703d062a80e808ea8d
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-16 18:06:10 -0300

    Corrige métodos de rota de senadores e legisladores para usar o método GET

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LegislatorController;
 
 Route::get('/deputies', [LegislatorController::class, 'indexForDeputies']);
-Route::post('/senators', [LegislatorController::class, 'indexForSenators']);
-Route::put('/legislators/{legislator}', [LegislatorController::class, 'show']);
+Route::get('/senators', [LegislatorController::class, 'indexForSenators']);
+Route::get('/legislators/{legislator}', [LegislatorController::class, 'show']);
