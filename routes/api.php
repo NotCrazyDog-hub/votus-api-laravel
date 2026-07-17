1c155b64cf1097449bf2d43b77368e36a78041fa
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-17 18:15:14 -0300

    Adiciona rotas para exibir deputados e senadores por ID externo

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LegislatorController;
 
 Route::get('/deputies', [LegislatorController::class, 'indexForDeputies']);
+Route::get('/deputies/{external_id}', [LegislatorController::class, 'showDeputy']);
 Route::get('/senators', [LegislatorController::class, 'indexForSenators']);
-Route::get('/legislators/{legislator}', [LegislatorController::class, 'show']);
+Route::get('/senators/{external_id}', [LegislatorController::class, 'showSenator']);
