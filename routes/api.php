64e880e08c0d1c108c9dd620a29f7c2a2f37d3c5
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-17 18:37:44 -0300

    Adiciona middleware de throttle para as rotas de deputados e senadores

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LegislatorController;
 
-Route::get('/deputies', [LegislatorController::class, 'indexForDeputies']);
-Route::get('/deputies/{external_id}', [LegislatorController::class, 'showDeputy']);
-Route::get('/senators', [LegislatorController::class, 'indexForSenators']);
-Route::get('/senators/{external_id}', [LegislatorController::class, 'showSenator']);
+Route::middleware('throttle:api')->group(function () 
+{
+    Route::get('/deputies', [LegislatorController::class, 'indexForDeputies']);
+    Route::get('/deputies/{external_id}', [LegislatorController::class, 'showDeputy']);
+    Route::get('/senators', [LegislatorController::class, 'indexForSenators']);
+    Route::get('/senators/{external_id}', [LegislatorController::class, 'showSenator']);
+});
