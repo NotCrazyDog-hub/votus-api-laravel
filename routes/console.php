3335dd2469cafe689fb5860e35b984b5fe9e1bbd
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-18 19:48:31 -0300

    Corrige sintaxe do agendamento para everyDays(8)

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,5 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Schedule::command('sync:legislators-lower-house')->everyEightDays();
-Schedule::command('sync:legislators-senate')->everyEightDays();
+Schedule::command('sync:legislators-lower-house')->everyDays(8);
+Schedule::command('sync:legislators-senate')->everyDays(8);
