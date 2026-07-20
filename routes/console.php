b412c02f7e9863d5a356abaec6d5021a7ab078a2
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-20 10:28:34 -0300

    Importa classe Schedule no console.php e remove importações desnecessárias

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('sync:legislators-lower-house')->cron('0 0 */8 * *');
 Schedule::command('sync:legislators-senate')->cron('0 0 */8 * *');
