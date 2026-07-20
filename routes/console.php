ce970ac0d96ecb0d6cbc2753e92cfc6936286aa2
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-20 10:40:12 -0300

    Altera agendamento de comandos para execução semanal

--- routes/console.php
+++ routes/console.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('sync:legislators-lower-house')->cron('0 0 */8 * *');
-Schedule::command('sync:legislators-senate')->cron('0 0 */8 * *');
+Schedule::command('sync:legislators-lower-house')->weekly();
+Schedule::command('sync:legislators-senate')->weekly();
