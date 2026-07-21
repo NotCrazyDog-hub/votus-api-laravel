8405ab9213e72f758f6e159e1cbe36c04fdc1475
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-21 11:40:39 -0300

    Adiciona agendamento e sincronização de comissões no console.php

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('sync:legislators-lower-house')->weekly();
+Schedule::command('sync:committees-lower-house')->weekly();
 Schedule::command('sync:legislators-senate')->weekly();
+Schedule::command('sync:committees-senate')->weekly();
