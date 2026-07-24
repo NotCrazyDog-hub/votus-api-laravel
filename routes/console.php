6a0cf0d10c2971f4218968121336e4afc480deb8
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-24 09:34:04 -0300

    Adiciona agendamentos para sincronização de proposições da câmara e do senado

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,5 @@ Schedule::command('sync:legislators-lower-house')->weekly();
 Schedule::command('sync:committees-lower-house')->weekly();
 Schedule::command('sync:legislators-senate')->weekly();
 Schedule::command('sync:committees-senate')->weekly();
+Schedule::command('sync:bills-lower-house')->weekly();
+Schedule::command('sync:bills-senate')->weekly();
