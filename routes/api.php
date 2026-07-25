ae86ca4e8d242cb4f9e08c82496a5aefac80e576
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-25 18:03:39 -0300

    Remove o rate limiting das rotas da API e simplifica as definições de rota

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\LegislatorController;
 use App\Http\Controllers\SchedulerController;
 
-Route::middleware('throttle:api')->group(function () 
-{
-    Route::get('/deputies', [LegislatorController::class, 'indexForDeputies']);
-    Route::get('/deputies/{external_id}', [LegislatorController::class, 'showDeputy']);
-    Route::get('/senators', [LegislatorController::class, 'indexForSenators']);
-    Route::get('/senators/{external_id}', [LegislatorController::class, 'showSenator']);
-});
+Route::get('/deputies', [LegislatorController::class, 'indexForDeputies']);
+Route::get('/deputies/{external_id}', [LegislatorController::class, 'showDeputy']);
+Route::get('/senators', [LegislatorController::class, 'indexForSenators']);
+Route::get('/senators/{external_id}', [LegislatorController::class, 'showSenator']);
 
 Route::get('/schedule/run/{token}', [SchedulerController::class, 'run']);
