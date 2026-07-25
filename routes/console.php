dcd427867100dc58b07f36c2e3c1da544c8b3238
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-25 16:53:14 -0300

    Altera a frequência das tarefas de sincronização para execução a cada minuto para testes

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('sync:legislators-lower-house')->weekly();
-Schedule::command('sync:committees-lower-house')->weekly();
-Schedule::command('sync:legislators-senate')->weekly();
-Schedule::command('sync:committees-senate')->weekly();
-Schedule::command('sync:bills-lower-house')->weekly();
-Schedule::command('sync:bills-senate')->weekly();
+Schedule::command('sync:legislators-lower-house')->everyMinute();
+Schedule::command('sync:committees-lower-house')->everyMinute();
+Schedule::command('sync:legislators-senate')->everyMinute();
+Schedule::command('sync:committees-senate')->everyMinute();
+Schedule::command('sync:bills-lower-house')->everyMinute();
+Schedule::command('sync:bills-senate')->everyMinute();
