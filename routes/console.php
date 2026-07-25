797f85c6b05485d3b79d25e5341524628154896d
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-25 17:43:38 -0300

    Altera ordem dos comandos Schedule em console.php

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('sync:legislators-lower-house')->everyMinute();
 Schedule::command('sync:committees-lower-house')->everyMinute();
+Schedule::command('sync:bills-lower-house')->everyMinute();
 Schedule::command('sync:legislators-senate')->everyMinute();
 Schedule::command('sync:committees-senate')->everyMinute();
-Schedule::command('sync:bills-lower-house')->everyMinute();
 Schedule::command('sync:bills-senate')->everyMinute();
