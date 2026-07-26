df686b110aca01b4f798ecf5d4237e1423899c28
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-26 15:31:15 -0300

    Comenta as tarefas agendadas no arquivo console.php e adiciona explicação sobre a descontinuação do uso

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('sync:legislators-lower-house')->everyFiveMinutes();
-Schedule::command('sync:committees-lower-house')->everyFiveMinutes();
-Schedule::command('sync:bills-lower-house')->everyFiveMinutes();
-Schedule::command('sync:legislators-senate')->everyFiveMinutes();
-Schedule::command('sync:committees-senate')->everyFiveMinutes();
-Schedule::command('sync:bills-senate')->everyFiveMinutes();
+//  Este arquivo foi descontinuado, 
+//  pois a execução de tarefas agendadas agora é feita através de um endpoint seguro na API,
+//  acessível apenas com um token válido.
+
+// Schedule::command('sync:legislators-lower-house')->everyFiveMinutes();
+// Schedule::command('sync:committees-lower-house')->everyFiveMinutes();
+// Schedule::command('sync:bills-lower-house')->everyFiveMinutes();
+// Schedule::command('sync:legislators-senate')->everyFiveMinutes();
+// Schedule::command('sync:committees-senate')->everyFiveMinutes();
+// Schedule::command('sync:bills-senate')->everyFiveMinutes();
