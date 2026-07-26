8535382555c4780f8bdc599206b6f080df9962da
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-26 11:19:17 -0300

    Altera a frequência das tarefas agendadas para sincronização de dados para a cada cinco minutos

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('sync:legislators-lower-house')->everyMinute();
-Schedule::command('sync:committees-lower-house')->everyMinute();
-Schedule::command('sync:bills-lower-house')->everyMinute();
-Schedule::command('sync:legislators-senate')->everyMinute();
-Schedule::command('sync:committees-senate')->everyMinute();
-Schedule::command('sync:bills-senate')->everyMinute();
+Schedule::command('sync:legislators-lower-house')->everyFiveMinutes();
+Schedule::command('sync:committees-lower-house')->everyFiveMinutes();
+Schedule::command('sync:bills-lower-house')->everyFiveMinutes();
+Schedule::command('sync:legislators-senate')->everyFiveMinutes();
+Schedule::command('sync:committees-senate')->everyFiveMinutes();
+Schedule::command('sync:bills-senate')->everyFiveMinutes();
