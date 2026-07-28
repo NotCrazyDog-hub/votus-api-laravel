3f97120206090c36d274553dd7460c11e6a814e1
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-07-28 16:36:27 -0300

    Atualiza as tarefas agendadas no console.php para incluir a opção withoutOverlapping, evitando sobreposições na execução.

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('sync:legislators-lower-house')->everyFiveMinutes();
-Schedule::command('sync:committees-lower-house')->everyFiveMinutes();
-Schedule::command('sync:bills-lower-house')->everyFiveMinutes();
-Schedule::command('sync:legislators-senate')->everyFiveMinutes();
-Schedule::command('sync:committees-senate')->everyFiveMinutes();
-Schedule::command('sync:bills-senate')->everyFiveMinutes();
+Schedule::command('sync:legislators-lower-house')->everyFiveMinutes()->withoutOverlapping();
+Schedule::command('sync:committees-lower-house')->everyFiveMinutes()->withoutOverlapping();
+Schedule::command('sync:bills-lower-house')->everyFiveMinutes()->withoutOverlapping();
+Schedule::command('sync:legislators-senate')->everyFiveMinutes()->withoutOverlapping();
+Schedule::command('sync:committees-senate')->everyFiveMinutes()->withoutOverlapping();
+Schedule::command('sync:bills-senate')->everyFiveMinutes()->withoutOverlapping();
