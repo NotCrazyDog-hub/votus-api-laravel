d484f66afacaae8ee1cf63876864962b1c0b4ee0
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-08-23 16:26:58 -0300

    Remove métodos de execução de comandos do SchedulerController e ajuste nas rotas

--- app/Http/Controllers/SchedulerController.php
+++ app/Http/Controllers/SchedulerController.php
@@ -7,86 +7,17 @@ use Illuminate\Support\Facades\Cache;
 
 class SchedulerController extends Controller
 {
-    private array $commands = [
-        'deputies' => 'sync:legislators-lower-house',
-        'senators' => 'sync:legislators-senate',
-        'committees-deputies' => 'sync:committees-lower-house',
-        'committees-senators' => 'sync:committees-senate',
-        'bills-deputies' => 'sync:bills-lower-house',
-        'bills-senators' => 'sync:bills-senate',
-    ];
-
-    public function runCommand(string $token, string $command)
-    {
-        if ($token !== config('app.scheduler_token')) {
-            abort(403);
-        }
-
-        if (! isset($this->commands[$command])) {
-            abort(404);
-        }
-
-        $artisanCommand = $this->commands[$command];
-        $logFile = storage_path("logs/schedule-{$command}.log");
-
-        $shellCommand = sprintf(
-            'nohup php artisan %s >> %s 2>&1 &',
-            escapeshellarg($artisanCommand),
-            escapeshellarg($logFile)
-        );
-
-        exec($shellCommand);
-
-        return response()->json([
-            'status' => 'started',
-            'command' => $artisanCommand,
-        ]);
-    }
-
-    // SchedulerController.php
-
-public function runCommandDebug(string $token, string $command)
-{
-    if ($token !== config('app.scheduler_token')) {
-        abort(403);
-    }
-
-    if (! isset($this->commands[$command])) {
-        abort(404);
-    }
-
-    $artisanCommand = $this->commands[$command];
-    $logFile = storage_path("logs/schedule-{$command}.log");
-    $basePath = base_path();
-
-    $shellCommand = sprintf(
-        'cd %s && nohup php artisan %s >> %s 2>&1 &',
-        escapeshellarg($basePath),
-        escapeshellarg($artisanCommand),
-        escapeshellarg($logFile)
-    );
-
-    exec($shellCommand);
-
-    return response()->json([
-        'status' => 'started',
-        'command' => $artisanCommand,
-    ]);
-}
+    // private array $commands = [
+    //     'deputies' => 'sync:legislators-lower-house',
+    //     'senators' => 'sync:legislators-senate',
+    //     'committees-deputies' => 'sync:committees-lower-house',
+    //     'committees-senators' => 'sync:committees-senate',
+    //     'bills-deputies' => 'sync:bills-lower-house',
+    //     'bills-senators' => 'sync:bills-senate',
+    // ];
 
     public function ping()
     {
         return response()->json(['status' => 'alive']);
     }
-
-    // public function run(string $token)
-    // {
-    //     if ($token !== config('app.scheduler_token')) {
-    //         abort(403);
-    //     }
-
-    //     Artisan::call('schedule:run');
-
-    //     return response()->json(['status' => 'scheduled tasks executed']);
-    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,4 @@ Route::get('/deputies/{external_id}', [LegislatorController::class, 'showDeputy'
 Route::get('/senators', [LegislatorController::class, 'indexForSenators']);
 Route::get('/senators/{external_id}', [LegislatorController::class, 'showSenator']);
 
-// Route::get('/schedule/run/{token}', [SchedulerController::class, 'run']);
-Route::get('/schedule/run/{token}/{command}', [SchedulerController::class, 'runCommand']);
-Route::get('/schedule/debug/{token}/{command}', [SchedulerController::class, 'runCommandDebug']);
 Route::get('/schedule/ping', [SchedulerController::class, 'ping']);
