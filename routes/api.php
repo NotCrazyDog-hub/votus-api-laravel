a9b090fcc144aa5a8ada631d374254f832a84042
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-08-25 11:56:09 -0300

    Revert "Merge branch 'news'"
    This reverts commit 3563fa038995eb3b1e002db3194d45fef703b32d, reversing
    changes made to bb91e3f7a4833ca89e76d35fb2be8181c7a657c9.

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LegislatorController;
 use App\Http\Controllers\SchedulerController;
-use App\Http\Controllers\NewsController;
 
 Route::get('/deputies', [LegislatorController::class, 'indexForDeputies']);
 Route::get('/deputies/{external_id}', [LegislatorController::class, 'showDeputy']);
 Route::get('/senators', [LegislatorController::class, 'indexForSenators']);
 Route::get('/senators/{external_id}', [LegislatorController::class, 'showSenator']);
-Route::get('/schedule/status', [SchedulerController::class, 'status']);
 
-Route::post('/noticias', [NewsController::class, 'store']);
-Route::get('/noticias', [NewsController::class, 'index']);
-Route::get('/noticias/{noticia}', [NewsController::class, 'show']);
+Route::get('/schedule/status', [SchedulerController::class, 'status']);
