552520bc33ed4a451cf4e77f3dd8aeb818afa1e9
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-08-27 10:47:28 -0300

    Altera a tabela do modelo News de 'noticias' para 'news'

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    protected $table = 'noticias';
+    protected $table = 'news';
 
     protected $fillable = [
         'title',
